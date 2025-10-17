a0a9d9b7e99a432a39a08639d62880cd0924f578
Juan Jose Jaramillo <juan.jaramillo14@eia.edu.co>
2025-10-17 13:45:43 -0500

    Se corrigen errores del back

--- backend/src/modules/merchant/merchant.controller.ts
+++ backend/src/modules/merchant/merchant.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { MerchantService } from './merchant.service';
 
-@Controller('merchant') // <--- base path
+@Controller('merchant') 
 export class MerchantController {
   constructor(private svc: MerchantService) {}
 
